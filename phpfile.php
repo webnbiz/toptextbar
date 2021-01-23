f7cba9b3c89bf6834f47febcc2ab93e7621a33ed
WenNbiz <41163558+webnbiz@users.noreply.github.com>
2021-01-23 05:41:28 +0530

    Create phpfile.php

---
+++ phpfile.php
@@ -0,0 +1,5 @@
+ for storefront theme
+ 
+ //Adding a top bar 
+ function amazing_club()
+{ ?> <div class="survey-wrapper"> <div class="survey-div"> <a href="#">Join the exclusive Online Deals Email list</a> </div> </div> <?php } add_action('storefront_before_site' , 'amazing_club');
